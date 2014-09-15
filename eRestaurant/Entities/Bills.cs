b2e462f6895f91385c2cb3641fdc43742b390664
tciarroni <tylerciarroni@gmail.com>
2014-09-15 16:18:20 -0600

    classes

---
+++ eRestaurant/Entities/Bills.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities
+{
+    public class Bills
+    {
+        public int BillID { get; set; }
+        public DateTime BillDate { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public int NumberInParty { get; set; }
+        public bool PaidStatus { get; set; }
+        public int WaiterID { get; set; }
+        public int TableID { get; set; }
+        public int ReservationID { get; set; }
+        public bool OrderReady { get; set; }
+        public string  Comment { get; set; }
+
+        public virtual ICollection<Items> Items {get;set;}
+        public virtual Waiters waiter {get;set;}
+        public virtual Table Table { get; set;}
+    }
+}
---
+++ eRestaurant/Entities/Items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities
+{
+    public class Items
+    {
+        public int ItemID { get; set; }
+        public string Description { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal CurrentCost { get; set; }
+        public bool Active { get; set; }
+        public int Calories { get; set; }
+        public string Comment { get; set; }
+        public int MenuCategoryID { get; set; }
+    }
+}
